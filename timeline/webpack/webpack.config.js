73ebd484d11e918fbac39663678fd8138ec4db8d
(none) <dandavison7@gmail.com>
2018-10-02 23:20:49 -0700

    Upgrade to current node

--- timeline/webpack/webpack.config.js
+++ timeline/webpack/webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'buble-loader' }
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
