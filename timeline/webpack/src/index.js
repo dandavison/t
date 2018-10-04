fb4d81090ae51ba0c2a22c01b5ef042d62f04408
(none) <dandavison7@gmail.com>
2018-10-03 22:21:27 -0700

    EventDrops example

--- timeline/webpack/src/index.js
+++ timeline/webpack/src/index.js
@@ -1,10 +1,30 @@
-import _ from 'lodash';
+import * as d3 from 'd3';
+
+import eventDrops from 'event-drops';
+
+const chart = eventDrops({ d3 });
+
+const repositoriesData = [
+    {
+        name: 'admin-on-rest',
+        data: [{ date: new Date('2014/09/15 14:21:31') } /* ... */],
+    },
+    {
+        name: 'event-drops',
+        data: [{ date: new Date('2014/09/15 13:24:57') } /* ... */],
+    },
+    {
+        name: 'sedy',
+        data: [{ date: new Date('2014/09/15 13:25:12') } /* ... */],
+    },
+];
 
 function component() {
   let element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  d3.select(element)
+    .data([repositoriesData])
+    .call(chart);
 
   return element;
 }
